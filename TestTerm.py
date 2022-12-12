9f19494ce7f29336676a5616c6cb9f70a3f5a38f
makennagall <68980127+makennagall@users.noreply.github.com>
2022-12-12 16:58:27 -0500

    Issues with infinite and zero types

--- TestTerm.py
+++ TestTerm.py
@@ -8,55 +8,68 @@ def main():
     #type variables cannot be numbers
     #VX.VY.(T1)->(T2)->FT
 #Term Parsing:
-    testType = sys.argv[1]
-    parenSplit = testType.split("(")
-    vars = parenSplit[0]
-    types = '('.join(parenSplit[1:])
-    print(types)
-    varList = []
-    for var in vars.split('V'):
-        if var != '':
-            var = var.replace(".", '')
-            varList.append(var)
-            #Dictionaries are ordered.
-    typeDict = {}
-    count = 0
-    for type in types.split(')')[:-1]:
-        type = type.replace("->(", '')
-        type = type.replace("->", '')
-        if count == 0:
-            firstTerm = type
-        if not (type in typeDict):
-            typeDict[type] = []
-        typeDict[type].append(count)
-        count+=1
-    finalType = types.split(')')[-1]
-    finalType = finalType.split('>')[1]
-    finalType = finalType.replace("->(", '')
-    typeDict[finalType] = ['']
-
-    print("starting Type: " + firstTerm)
-    print("final type: " + finalType)
-    print("starting dictionary: ", end='')
-    print(typeDict)
-#Check infinite:
-    typeDict = genTerms(typeDict, varList)
-    print("Dictionary after terms generated: ", end='')
-    print(typeDict)
-    for type in typeDict:
-        if checkInfinite(type, typeDict) == True:
-            sys.exit("Infinite Term")
-    finalList = substitute(typeDict, firstTerm, [''])
-    #guarantee unique values using set:
-    finalSet = set()
-    for term in finalList:
-        finalSet.add(term)
-    print("Number of inhabitants: ", end='')
-    print(len(finalSet))
-    print("List of inhabitants: ")
-    for term in finalSet:
-        print(term)
-
+    testFileName = sys.argv[1]
+    testFile = open(testFileName, "r")
+    for line in testFile:
+        line = line[:-2]
+        line = line[1:]
+        #parse term, evaluate term
+        print(line)
+        typeDict, varList, firstTerm, finalType = parseType(line)
+        #print("starting Type: " + firstTerm)
+        #print("final type: " + finalType)
+        lambdaTerm = makeLambdaTerm(varList, typeDict)
+        print(lambdaTerm)
+    #Check infinite:
+        typeDict = genTerms(typeDict, varList)
+        if not finalType in typeDict:
+            typeDict[finalType] = ['']
+        print(typeDict)
+        termIsInfinite = False
+        for type in typeDict:
+            if checkInfinite(type, typeDict) == True:
+                termIsInfinite = True
+        if not termIsInfinite:
+            finalList, nonZero = substitute(typeDict, firstTerm, [''])
+            #guarantee unique values using set:
+            if nonZero == True:
+                finalSet = set()
+                for term in finalList:
+                    finalSet.add(term)
+                print("Number of inhabitants: ", end='')
+                print(len(finalSet))
+                print("List of inhabitants: ")
+                for term in finalSet:
+                    print(term)
+            else:
+                print(finalList)
+            print()
+def parseType(testType):
+        parenSplit = testType.split("(")
+        vars = parenSplit[0]
+        types = '('.join(parenSplit[1:])
+        varList = []
+        for var in vars.split('V'):
+            if var != '':
+                var = var.replace(".", '')
+                varList.append(var)
+                #Dictionaries are ordered.
+        typeDict = {}
+        count = 0
+        for type in types.split(')')[:-1]:
+            type = type.replace("->(", '')
+            type = type.replace("->", '')
+            if count == 0:
+                firstTerm = type
+            if not (type in typeDict):
+                typeDict[type] = []
+            typeDict[type].append(count)
+            count+=1
+        finalType = types.split(')')[-1]
+        finalType = finalType.split('>')[1]
+        finalType = finalType.replace("->(", '')
+        #typeDict[finalType] = ['']
+        return typeDict, varList, firstTerm, finalType
 def checkInfinite(term, typeDict):
     termOutcome = term[-1]
     for var in term[:-1]:
@@ -64,8 +77,8 @@ def checkInfinite(term, typeDict):
             #check to make sure the term isn't uninhabited:
             for variable in list(term):
                 if not variable in typeDict:
-                    print("Impossible to make any terms")
-                    sys.exit("Uninhabited")
+                    print("Uninhabited")
+                    return 'Uninhabited'
             return True
     else:
         return False
@@ -73,12 +86,17 @@ def substitute(typeDict, startTerm, finalList):
     for var in list(startTerm):
         newList = []
         if not var in typeDict:
-            return "error, missing term: " + var
+            returnString = "Uninhabited Term"
+            return returnString, False
         for term in finalList:
             for newTerm in typeDict[var]:
-                newList.append(term + newTerm)
+                newList.append(str(term) + str(newTerm))
         finalList = newList
-    return finalList
+    if finalList == []:
+        returnString = "Uninhabited Term"
+        return returnString, False
+    else:
+        return finalList, True
 def genTerms(typeDict, varList):
     tryAgainList = []
     for var in varList:
@@ -88,7 +106,6 @@ def genTerms(typeDict, varList):
                 tryAgainList = newList
             else:
                 typeDict[var] = newList
-
     while tryAgainList != []:
         for var in tryAgainList:
             newList, newTermMade = genTermsHelper(typeDict, var, tryAgainList)
@@ -113,8 +130,20 @@ def genTermsHelper(typeDict, var, tryAgainList):
                 tryAgainList.append(var)
                 return tryAgainList, False
     #make sure it doesn't run infinitely:
-    tryAgainList.remove(var)
+    if var in tryAgainList:
+        tryAgainList.remove(var)
     return tryAgainList, False
+
+def makeLambdaTerm(varList, typeDict):
+    lambdaTerm = ''
+    for var in varList:
+        lambdaTerm = lambdaTerm + 'V' + var + '.'
+    for type in typeDict:
+        if typeDict[type] != ['']:
+            for num in typeDict[type]:
+                lambdaTerm = lambdaTerm + "\\" + str(num) + ":(" + '->'.join(list(type)) + ")."
+
+    return lambdaTerm
 
 #Execute Main:
 if __name__ == '__main__':
